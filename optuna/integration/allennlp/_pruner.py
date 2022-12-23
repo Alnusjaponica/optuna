ce8c09bcd36e5a049bc7c1e923864f88c78656cf
Alnusjaponica <50256998+Alnusjaponica@users.noreply.github.com>
2022-12-23 13:25:53 +0900

    Fix mypy annotation in allennlp/_ptuner.py

--- optuna/integration/allennlp/_pruner.py
+++ optuna/integration/allennlp/_pruner.py
@@ -31,7 +31,7 @@ else:
     # I disable mypy here since `allennlp.training.TrainerCallback` is a subclass of `Registrable`
     # (https://docs.allennlp.org/main/api/training/trainer/#trainercallback) but `TrainerCallback`
     # defined here is not `Registrable`, which causes a mypy checking failure.
-    class TrainerCallback:  # type: ignore
+    class TrainerCallback:  # type: ignore[no-redef]
         """Stub for TrainerCallback."""
 
         @classmethod
@@ -116,7 +116,7 @@ class AllenNLPPruningCallback(TrainerCallback):
     ):
         _imports.check()
 
-        if version.parse(allennlp.__version__) < version.parse("2.0.0"):  # type: ignore
+        if version.parse(allennlp.__version__) < version.parse("2.0.0"):
             raise ImportError(
                 "`AllenNLPPruningCallback` requires AllenNLP>=v2.0.0."
                 "If you want to use a callback with an old version of AllenNLP, "
